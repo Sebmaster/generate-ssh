cfbc8ad225d14d72c78f4097d429d96ad02563e1
Sebastian Mayr <sebmaster16@gmail.com>
2015-04-24 15:47:15 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+﻿generate-ssh
+###
+
+generate-ssh is a small wrapper around the ssh-keygen tool.
+It supports proper error handling, doesn't call the callback multiple times (:<) and has some support for locating the ssh-keygen executable on Windows.
+
+generate(opts { additionalPaths, comment, bits }, cb(err, data{ private, public }))
+---
+
+The generate function internally automatically tries to locate the ssh-keygen binary. It searches for the executable in `PATH` and in some commonly found places on Windows.
+Alternatively, you can provide the `additionalPaths` option. It's an array with paths to search in.
+
+You can also provide the `comment` and `bits` option, which modify how the ssh-keygen binary is called.
+
+The generated private and public keys are provided to the callback in an object with `private` and `public` keys.
+
+locate(searchPaths, cb(err, location))
+---
+
+locate takes an array `searchPaths` and tries to find the ssh-keygen executable in them.
+If it does find them, it calls the callback with the final resolved path as the location argument. If it doesn't the callback is called with an error.
---
+++ index.js
@@ -0,0 +1,143 @@
+﻿"use strict";
+
+var os = require("os");
+var path = require("path");
+var fs = require("fs");
+var spawn = require("child_process").spawn;
+
+var which = require("which");
+var temp = require("temp");
+
+var additionalPaths = {
+  win32: [
+    "C:\\Program Files (x86)\\Git\\bin",
+    "C:\\Program Files\\Git\\bin"
+  ]
+};
+
+function findValidExecutable(paths, cb) {
+  if (paths.length === 0) {
+    return cb(new Error("Couldn't locate executable at given paths."));
+  }
+
+  var loc = paths.shift();
+  
+  fs.stat(loc, function (err, stat) {
+    if (stat && stat.isFile()) {
+      cb(null, loc);
+    } else if (paths.length !== 0) {
+      findValidExecutable(paths, cb);
+    } else {
+      cb(new Error("Couldn't locate executable at given paths."));
+    }
+  });
+}
+
+module.exports.locate = function (searchPaths, cb) {
+  var cmd = "ssh-keygen";
+  
+  which(cmd, function (err, res) {
+    if (res) return cb(null, res);
+    
+    var execPaths = [];
+    
+    if (process.platform === "win32") {
+      var extensions = process.env.PATHEXT.split(";");
+      if (cmd.indexOf(".") !== -1) {
+        extensions.unshift("");
+      }
+      
+      for (var i = 0; i < searchPaths.length; ++i) {
+        var execBase = path.resolve(searchPaths[i], cmd);
+        
+        for (var j = 0; j < extensions.length; ++j) {
+          var execLoc = execBase + extensions[j];
+          
+          execPaths.push(execLoc);
+        }
+      }
+    } else {
+      execPaths = searchPaths.map(function (loc) {
+        return path.resolve(loc, cmd);
+      });
+    }
+    
+    findValidExecutable(execPaths, cb);
+  });
+};
+
+module.exports.generate = function (opts, cb) {
+  if (typeof opts === "function") {
+    cb = opts;
+    opts = {};
+  }
+  
+  opts.additionalPaths = opts.additionalPaths || additionalPaths[process.platform] || [];
+  
+  module.exports.locate(opts.additionalPaths, function (err, loc) {
+    if (err) return cb(err);
+    
+    var keyPath = temp.path();
+    
+    var args = ["-q", "-t", "rsa", "-f", keyPath, "-N", ""];
+    if (opts.comment !== undefined) {
+      args.push("-C", opts.comment);
+    }
+    if (opts.bits) {
+      args.push("-b", opts.bits);
+    }
+    
+    var done = false;
+    var cp = spawn(loc, args, { stdio: "ignore" });
+    
+    cp.on("error", function () {
+      fs.unlink(keyPath, function () { });
+      fs.unlink(keyPath + ".pub", function () { });
+      
+      if (done) return;
+      
+      done = true;
+      cb(new Error("An error occured while executing ssh-keygen."));
+    });
+    
+    cp.on("close", function (code, signal) {
+      if (done) {
+        fs.unlink(keyPath, function () { });
+        fs.unlink(keyPath + ".pub", function () { });
+        
+        return;
+      }
+      
+      done = true;
+      
+      fs.readFile(keyPath, { encoding: "utf8" }, function (err, privateKey) {
+        if (err) {
+          fs.unlink(keyPath, function () { });
+          fs.unlink(keyPath + ".pub", function () { });
+          done = true;
+          
+          return cb(err);
+        }
+        
+        fs.readFile(keyPath + ".pub", { encoding: "utf8" }, function (err, publicKey) {
+          if (err) {
+            fs.unlink(keyPath, function () { });
+            fs.unlink(keyPath + ".pub", function () { });
+            done = true;
+            
+            return cb(err);
+          }
+          
+          done = true;
+          fs.unlink(keyPath, function () { });
+          fs.unlink(keyPath + ".pub", function () { });
+          
+          cb(null, {
+            private: privateKey,
+            public: publicKey
+          })
+        });
+      });
+    });
+  });
+};
